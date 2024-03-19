3870592eb18ed4447b7c21c8c91d40e87f1da3d9
Renatuscape <rrjonsberg@gmail.com>
2024-03-19 14:40:36 +0100

    Added state in App to track custom names. Player name is now displayed on victory.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function HandleEditClick() {
         setIsEditing(wasEditing => !wasEditing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     let editablePlayerName = <span className='player-name'>{playerName}</span>;
